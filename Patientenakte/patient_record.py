cc691c9f5917e44dc1ff6081b58ffc5690947988
licht2050 <mustaga.balkhi.mb@gmail.com>
2023-08-08 13:48:57 +0000

    weiter Generatoren sind implementiert

--- Patientenakte/patient_record.py
+++ Patientenakte/patient_record.py
@@ -2,6 +2,8 @@
 
 import random
 import time
+import csv
+import os
 from kafka import KafkaProducer
 
 class PatientRecordGenerator:
@@ -12,11 +14,33 @@ class PatientRecordGenerator:
         self.female_names = ["Maria", "Elisabeth", "Ruth", "Hilde", "Eva", "charlotte", "Erika"]
         self.genders= ["weiblich", "männlich"]
         self.streets = ["Banhofstraße 1", "Goebenstraße 40", "Am Rastpfuhl 3", "St. Johanner Str.", "Breite Str."]
-        self.cities = ["Saarbrücken"]
+        self.cities = ["Saarbreucken"]
         self.postal_codes = [66113, 66117, 6123, 66111]
         self.blood_groups = ["A+", "A-", "B+", "B-", "AB+", "AB-", "0+", "0-"]
         self.weight_range = (45, 120)
         self.height_range = (140, 195)
+        self.load_existing_ids()
+
+        # ...
+
+    def load_existing_ids(self):
+        try:
+            with open('patient_ids.csv', 'r') as file:
+                reader = csv.reader(file)
+                existing_ids = set(row[0] for row in reader)
+            return existing_ids
+        except FileNotFoundError:
+            return set()
+        
+    def save_new_id(self, patient_id):
+        try:
+            mode = 'a' if os.path.exists('patient_ids.csv') else 'w'
+            with open('patient_ids.csv', mode) as file:
+                writer = csv.writer(file)
+                writer.writerow([patient_id])
+        except Exception as e:
+            print("Error:", e)   
+
 
     def generate_random_birth_date(slef):
         now = int(time.time())
@@ -26,11 +50,13 @@ class PatientRecordGenerator:
         return birth_date
 
     def generate_random_patient_id(self):
-        # Erzeugt eine zufällige 6-stellige ID und überprüft, ob sie bereits existiert
+        # Lade vorhandene Patienten-IDs aus der CSV-Datei
+        existing_ids = self.load_existing_ids()
+        # Generiere eine zufällige 6-stellige ID und überprüfe, ob sie bereits existiert
         while True:
-            patient_id = str(random.randint(100000, 999999))
-            if patient_id not in self.patient_ids:
-                self.patient_ids.add(patient_id)
+            patient_id = str(random.randrange(100000, 1000000))
+            if patient_id not in existing_ids:
+                self.save_new_id(patient_id)
                 return patient_id
 
     def generate_random_patient_record(self):  
@@ -71,34 +97,34 @@ class PatientRecordGenerator:
         return patient_record
     
 
-def send_patient_record(producer, topic, interval=5):
-    generator = PatientRecordGenerator()
+# def send_patient_record(producer, topic, interval=5):
+#     generator = PatientRecordGenerator()
 
-    try:
-        while True:
-            patient_record = generator.generate_random_patient_record()
-            message = f"{patient_record}"
+#     try:
+#         while True:
+#             patient_record = generator.generate_random_patient_record()
+#             message = f"{patient_record}"
 
-            producer.send(topic, value=str(message).encode('utf-8'))
-            print(patient_record)
-            time.sleep(5)
-    except KeyboardInterrupt:
-        print("Patient record generator stopped.")
-    finally:
-        producer.flush()
+#             producer.send(topic, value=str(message).encode('utf-8'))
+#             print(patient_record)
+#             time.sleep(5)
+#     except KeyboardInterrupt:
+#         print("Patient record generator stopped.")
+#     finally:
+#         producer.flush()
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    bootstrap_server = "localhost:9092"
-    topic = "Patientenakte"
+#     bootstrap_server = "localhost:9092"
+#     topic = "Patientenakte"
 
-    producer = KafkaProducer(bootstrap_servers= bootstrap_server)
-    try:
-        send_patient_record(producer, topic)
-    except Exception as e:
-        print("Error: {e}")
-    finally:
-        producer.close()
+#     producer = KafkaProducer(bootstrap_servers= bootstrap_server)
+#     try:
+#         send_patient_record(producer, topic)
+#     except Exception as e:
+#         print("Error: {e}")
+#     finally:
+#         producer.close()
     
 
 
--- create_topic.py
+++ create_topic.py
@@ -12,10 +12,34 @@ def create_topic(bootstrap_servers, topic_name, partitions=1, replication_factor
     except Exception as e:
         print(f"Error occurred while creating the topic: {e}")
 
+def create_topics(bootstrap_servers, topic_configs):
+    try:
+        admin_client = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
+        new_topics = [NewTopic(name=config['topic_name'], num_partitions=config['partitions'], replication_factor=config['replication_factor']) for config in topic_configs]
+        admin_client.create_topics(new_topics=new_topics, validate_only=False)
+        for config in topic_configs:
+            print(f"Topic '{config['topic_name']}' created successfully.")
+    except TopicAlreadyExistsError as e:
+        print(f"Topic '{e.args[0]}' already exists.")
+    except Exception as e:
+        print(f"Error occurred while creating topics: {e}")
+
 if __name__ == "__main__":
     bootstrap_servers = "localhost:9092"  
-    topic_name = "RoomStatus"          
-    partitions = 3                        
-    replication_factor = 1 
+    # topic_name = "RoomStatus"          
+    # partitions = 3                        
+    # replication_factor = 1 
 
-    create_topic(bootstrap_servers, topic_name, partitions, replication_factor)
+    # create_topic(bootstrap_servers, topic_name, partitions, replication_factor)
+
+    topic_configs = [
+        {"topic_name": "RoomStatus", "partitions": 3, "replication_factor": 2},
+        {"topic_name": "SensorData", "partitions": 5, "replication_factor": 2},
+        {"topic_name": "PatientenRecords", "partitions": 3, "replication_factor": 2},
+        {"topic_name": "EmergencyAlerts", "partitions": 1, "replication_factor": 1},
+        {"topic_name": "StaffCommunication", "partitions": 5, "replication_factor": 2},
+        {"topic_name": "InventoryManagement", "partitions": 3, "replication_factor": 2}
+        
+    ]
+
+    create_topics(bootstrap_servers, topic_configs)
