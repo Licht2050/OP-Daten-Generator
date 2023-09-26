3f4aa7914c8e66263e4114c3a561619f64daf53e
licht2050 <mustaga.balkhi.mb@gmail.com>
2023-09-26 09:37:16 +0000

    cleaning

--- ingestors/API/pub_sub/event_loop.py
+++
@@ -1,4 +0,0 @@
-import asyncio
-
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
--- ingestors/op_api/api/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-def subscribe_indoor_environment(request):
-    # Your view logic here
-    return HttpResponse("Hello, world!")
